d7330991c0b35f73f2363466a465be25b4650016
AdsGab <gabrielleadsf@gmail.com>
2024-02-27 15:07:51 +0700

    Menambahakan nama

--- Le Practicio Week 2/Program.cs
+++ Le Practicio Week 2/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Masukan nama Anda:");
+String namaKami = Console.ReadLine();
 Console.WriteLine("Hello, World!");
+
+bool fish = true;
+if (fish)
+{
+    Console.WriteLine("WHAT?!");
+}
